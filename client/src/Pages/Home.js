7b0fb94837166d3bbc81f6ec9e9b67dd55f12526
anju <anjudh5802@gmail.com>
2022-05-25 13:41:38 -0400

    wrapped with default page component

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import DefaultPage from '../Component/DefaultPage'
 
 export default function Home() {
   return (
-    <div>
-        <h1>Home page</h1>
-    </div>
+    <DefaultPage>
+ <h1>Home page</h1>
+    </DefaultPage>
+       
+   
   )
 }
