29c68ae96c4d4bcbfc560acffe87f0326826e794
anju <anjudh5802@gmail.com>
2022-05-25 12:56:48 -0400

    route added for the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import CarBooking from './Pages/CarBooking';
 
 function App() {
   return (
     <div className="App">
-        <h1>Home Page</h1>
+        <Routes>
+          <Route exact path='/'  element={<Home/>} />
+          <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/register' element={<Register/>} />
+          <Route exact path='/carbooking' element={<CarBooking/>} />
+      </Routes>
     </div>
   );
 }
