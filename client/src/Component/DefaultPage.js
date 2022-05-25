95b53194923a56c624bf6c8e3fb1de5ff002fa43
anju <anjudh5802@gmail.com>
2022-05-25 13:40:29 -0400

    added bootstrap class

--- client/src/Component/DefaultPage.js
+++ client/src/Component/DefaultPage.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function DefaultPage(props) {
   return (
     <div>
-        <div className='header'>
+        <div className='header heading'>
             <div className='d-flex justify-content-between'>
-                <h2>Book Ridez</h2>
+                <h2>Car Rental</h2>
+            </div>
             </div>
             <div className='content'>
 {props.children}
 
-            </div>
         </div>
     </div>
   )
