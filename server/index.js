0c166ec26abeb14ed0b6ce4f6c85285585b0f5d3
anju <anjudh5802@gmail.com>
2022-05-25 17:01:03 -0400

    db connection added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port =process.env.PORT || 8000
+const dbconnection = require('./db')
 
 app.get('/',(req, res) => res.send("Hello World"))
 app.listen(port, () => console.log(`running server in ${port}`))
