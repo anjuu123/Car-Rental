f872e06554524c62b8a2e2d752fa87450bf57808
anju <anjudh5802@gmail.com>
2022-05-26 14:58:17 -0400

    accessing reducer data

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import DefaultPage from '../Component/DefaultPage'
+import { useSelector } from 'react-redux'
 
 export default function Home() {
+  const {cars} = useSelector(state=>state.carReducer)
   return (
     <DefaultPage>
  <h1>Home page</h1>
+ <h1>The length of cars array is{cars.length}</h1>
     </DefaultPage>
        
    
