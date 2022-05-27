bb4b1577d7f81edf6fb910e9389f12fbe7407ab5
anju <anjudh5802@gmail.com>
2022-05-27 16:11:46 -0400

    login form with required field

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Row, Col, Form, Input, Button } from 'antd';
 
-export default function Login() {
+function Login() {
   return (
-    <div>
-        <h1>Login Page</h1>
+    <div className='login'>
+      <Row gutter={16} className="d-flex align-item-center">
+        <Col lg={16} style={{ position: 'relative' }}>
+
+          <img src='https://s3.amazonaws.com/cka-dash/030-0220-CCF910/model2.png' alt='img-car' />
+        <h1 className='login-logo'>A&A Rental</h1>
+        </Col>
+        <Col lg={8} className='text-left'>
+
+
+          <Form layout="vertical" className='login-form'>
+            <h1>Login</h1>
+            <hr />
+
+            <Form.Item name="username" label="Username" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+
+            <Form.Item name="password" label="Password" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+
+            <Button className='btn1'>Login</Button>
+          </Form>
+        </Col>
+      </Row>
+
     </div>
   )
 }
+
+
+export default Login
