413708ce27363cdf220e887ff457363aca2d3947
anju <anjudh5802@gmail.com>
2022-05-26 23:16:40 -0400

    mapped cars data to display in home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import DefaultPage from '../Component/DefaultPage'
-import { useSelector } from 'react-redux'
+import { getAllCars } from '../redux/actions/carsAction.js'
+import {Row, Col} from 'antd'
 
 export default function Home() {
-  const {cars} = useSelector(state=>state.carReducer)
+  const { cars, loading } = useSelector(state => state.carReducer)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllCars())
+  }, [])
+
+
   return (
     <DefaultPage>
- <h1>Home page</h1>
- <h1>The length of cars array is{cars.length}</h1>
+      <Row>
+        {cars.map(car=>{
+          return
+           <Col lg={5} sm={24} xs={24}>
+          <div>
+            <h3>{car.name}</h3>
+          </div>
+          </Col>
+        })}
+      </Row>
+      
     </DefaultPage>
-       
-   
+
+
   )
 }
+
