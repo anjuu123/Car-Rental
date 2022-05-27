2829d4447f1cbfa10cb8bce3c6ebcc9e09aa0004
anju <anjudh5802@gmail.com>
2022-05-26 23:21:27 -0400

    added routes for cars

--- server/index.js
+++ server/index.js
@@ -3,5 +3,7 @@ const app = express()
 const port =process.env.PORT || 8000
 const dbconnection = require('./db')
 
+
+app.use('/api/cars/', require('./routes/carRoutes'))
 app.get('/',(req, res) => res.send("Hello World"))
 app.listen(port, () => console.log(`running server in ${port}`))
