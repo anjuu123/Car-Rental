c0a21fb68671452f17229a594a16f427a39a1ea8
anju <anjudh5802@gmail.com>
2022-05-27 17:03:20 -0400

    register form is created

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import {Row,Col,Input,Form} from 'antd';
+import { Link } from 'react-router-dom';
 
-export default function Register() {
-  return (
-    <div>
-        <h1>Register Page</h1>
-    </div>
-  )
+function onFinish(values){
+  console.log(values)
 }
+
+function Register() {
+  return (
+    <div className='login'>
+    <Row gutter={16} className="d-flex align-item-center">
+      <Col lg={16} style={{ position: 'relative' }}>
+
+        <img src='https://s3.amazonaws.com/cka-dash/030-0220-CCF910/model2.png' alt='img-car' />
+      <h1 className='login-logo'>A&A Rental</h1>
+      </Col>
+      <Col lg={8} className='text-left'>
+
+
+        <Form layout="vertical" className='login-form p-2' onFinish={onFinish}>
+          <h1>Register</h1>
+          <hr />
+
+          <Form.Item name="username" label="Username" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="password" label="Password" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="Confirm password" label="Confirm Password" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+
+          <button className='btn1 mt-2 mb-3'>Register</button>
+          <br/>
+
+         <Link to='/login'>Already have an account ? Click Here</Link>
+        </Form>
+      </Col>
+    </Row>
+
+  </div>
+)
+}
+
+
+export default Register
